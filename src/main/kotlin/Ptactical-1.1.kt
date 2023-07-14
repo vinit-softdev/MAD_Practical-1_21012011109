abc0cc08c92c875c7fbc5ae9115471fb68cb6cac
student <student@gnu.ac.in>
2023-07-14 08:29:15 +0530

    Practical_1

---
+++ src/main/kotlin/Ptactical-1.1.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var a:Int=5
+    var b:Float=2.2f
+    var c:Char='t'
+    var d:String="vinit"
+    var e:Boolean=true
+    var f:Double=12.20
+    var g:Long=1255221221122
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+}
