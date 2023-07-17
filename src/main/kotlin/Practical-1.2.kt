0a143e82715614815eb554b9309ce7f332d2060b
vinit-softdev <vinithpatel21@gnu.ac.in>
2023-07-17 21:22:29 +0530

    question 2 and 3 done

---
+++ src/main/kotlin/Practical-1.2.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var a=10
+    var b=a.toDouble()
+    var c="10"
+    var d=c.toInt()
+    var e="434.334"
+    var f=e.toDouble()
+
+    println("Integer Value:$a")
+    println("Double Value:$b")
+    println("String Value:$c")
+    println("Integer Value:$d")
+    println("String Value:$e")
+    println("Double Value:$f")
+
+}
---
+++ src/main/kotlin/Practical-1.3.kt
@@ -0,0 +1,26 @@
+fun main(){
+    println("Enter Enrollment No:")
+    var en= readLine()
+    println("Enter the Name:")
+    var name= readLine()
+    println("Enter the Branch:")
+    var branch= readLine()
+    println("Enter the Class:")
+    var class1= readLine()
+    println("Enter the College Name:")
+    var cname= readLine()
+    println("Enter the University Name:")
+    var uname= readLine()
+    println("Enter the Age:")
+    var age= readLine()
+
+    println("Enrollment No:$en")
+    println("Name:$name")
+    println("Branch:$branch")
+    println("Class:$class1")
+    println("College:$cname")
+    println("University:$uname")
+    println("Age:$age")
+
+
+}
