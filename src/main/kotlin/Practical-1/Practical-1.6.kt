89cdd35d1df670fdfbf841ac11a891c098cfbe68
student <student@gnu.ac.in>
2023-07-18 08:43:31 +0530

    practical 6 complete

---
+++ src/main/kotlin/Practical-1/Practical-1.6.kt
@@ -0,0 +1,21 @@
+fun main(){
+
+    val number1 = 12
+    val number2 = 3
+    var result: Int
+
+    result = number1 + number2
+    println("number1 + number2 = $result")
+
+    result = number1 - number2
+    println("number1 - number2 = $result")
+
+    result = number1 * number2
+    println("number1 * number2 = $result")
+
+    result = number1 / number2
+    println("number1 / number2 = $result")
+
+    result = number1 % number2
+    println("number1 % number2 = $result")
+}
